cc411ebd232f413647d266f61cf57cdfc7248d9e
FukaOno <atopfuka@gmail.com>
2025-01-23 22:29:48 -0500

    update api folder

--- api_endpoints/2019-2020.py
+++ api_endpoints/2019-2020.py
@@ -1,3 +1,14 @@
 from flask import Flask, jsonify, request
 import mysql.connector 
 from mysql.connector import Error
+
+app = Flask(__name__)
+
+db_config= {
+    "host": "3306",
+    "user": "root", 
+
+    
+}
+
+#to be continued
